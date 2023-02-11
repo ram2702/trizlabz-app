e4386969b384ee1e6dfbdcc97dee96987bba6f1e
RAMSANKAR. I <ramiyyanath@gmail.com>
2023-02-11 18:20:47 +0530

    v.1.4.1

--- src/auth/keycloak.js
+++ src/auth/keycloak.js
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 (function (root, factory) {
   if (typeof exports === "object") {
     if (typeof module === "object") {
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "../css/login.css";
 import logo from "../img/Trizlabz_logo__1_-removebg-preview 1.png";
+import login1 from "../img/login-bg.png";
+import BackgroundSlider from "react-background-slider";
 export default function Login() {
   return (
     <div className="log-cont">
       <div className="log-box">
         <article className="box-1">
-          <h1>Inventive Thinking Minds!</h1>
+          <span className="head-cont">
+            <h1>Inventive Thinking Minds!</h1>
+          </span>
           <p>
             Inventive problem solving combined with technology agility to create
             reliable, sustainable and optimal designs for our products and
